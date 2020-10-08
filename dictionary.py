c5db9930c6bcdc8fe0c8aac82b64d2effba80369
Maya Salcedo <68334235+maya-salcedo@users.noreply.github.com>
2020-10-08 08:24:35 +0300

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,12 +1,20 @@
 import json
+import difflib
 
 data = json.load(open('data.json'))
 
 def definition(word):
-    if word in data:
-        return(data[word])
-    else:
-        return('That does not exist in my dictionary. Sorry.')
+    while True:
+        if word in data:
+            return(data[word])
+        else:
+            try:
+                alternative_word = difflib.get_close_matches(word, data, n=1)
+                word = alternative_word[0]
+                return("Do you mean " + word + "?\n" + str(data[word]))
+            except IndexError:
+                return('that is not in my dictionary. sorry')
+                break
 
 while True:
         word_to_check = input('Enter a word:').strip().lower()
